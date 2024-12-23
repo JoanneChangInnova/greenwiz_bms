a59e6e3384a9d8c5e56aae607436089eaa58153d
JoanneChangInnova <Joanne.Chang@innovasolutions.com>
2024-12-23 23:26:10 +0800

    feat: init

---
+++ src/main/java/com/greenwiz/bms/BmsApplication.java
@@ -0,0 +1,13 @@
+package com.greenwiz.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsApplication.class, args);
+	}
+
+}
