9bd9de5325019d724679719a65a8985e383955fa
JoanneChangInnova <Joanne.Chang@innovasolutions.com>
2025-04-05 13:25:57 +0800

    feat: userData display text

--- src/main/java/com/greenwiz/bms/controller/data/user/UserData.java
+++ src/main/java/com/greenwiz/bms/controller/data/user/UserData.java
@@ -13,4 +13,16 @@ public class UserData {
         this.username = username;
         this.email = email;
     }
+
+    private String displayText;
+
+    // 在建構函數或 setter 方法中設置 displayText
+    public void setDisplayText() {
+        this.displayText = this.username + " (" + this.email + ")";
+    }
+
+    // 或者使用 getter 方法動態生成
+    public String getDisplayText() {
+        return this.username + " (" + this.email + ")";
+    }
 }
